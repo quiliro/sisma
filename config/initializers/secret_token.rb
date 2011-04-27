b3931d7954b8e0b8bbb037c83d88fc2458edc714
Quiliro Ordóñez <quiliro@fsfla.org>
2011-04-27 07:50:03 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sisma::Application.config.secret_token = 'c06158c4e1ea18f4bc73f3229af18dea4a6111645e7da1784455e635465561339bd700731f8dc4720decce48ff2dd30aea2a688cdfc30d201fc6431214b81da5'
